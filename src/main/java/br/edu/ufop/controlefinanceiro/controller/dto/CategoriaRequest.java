d36d48c598b68da19e84f0a95c954783f59c20ec
Jonass-C <185560921+Jonass-C@users.noreply.github.com>
2026-07-21 22:19:16 -0300

    refactor: atualiza docs do BD, desativa dev_mode e remove codigo morto
    - atualiza diagrama ER em docs/bd
    - desativa app.security.dev-mode em application.properties
    - remove import desnecessário em CategoriaRequest.java
    - apaga comentário em cadastro.html

--- src/main/java/br/edu/ufop/controlefinanceiro/controller/dto/CategoriaRequest.java
+++ src/main/java/br/edu/ufop/controlefinanceiro/controller/dto/CategoriaRequest.java
@@ -1,7 +1,6 @@
 package br.edu.ufop.controlefinanceiro.controller.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
